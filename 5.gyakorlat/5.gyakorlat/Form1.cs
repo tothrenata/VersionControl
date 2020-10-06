c45e830c37d6b99f04d7f463dcc3e4b83e45c269
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-10-06 17:17:59 +0200

    Portfólió tárolása a memóriában

--- 5.gyakorlat/5.gyakorlat/Form1.cs
+++ 5.gyakorlat/5.gyakorlat/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _5.gyakorlat.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,11 +15,22 @@ namespace _5.gyakorlat
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
