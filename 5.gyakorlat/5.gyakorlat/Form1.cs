4e5eb5bc5706f520d968f09063acf645e3f0655c
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-10-06 17:08:38 +0200

    Adatok betöltése

---
+++ 5.gyakorlat/5.gyakorlat/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5.gyakorlat
+{
+    public partial class Form1 : Form
+    {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        public Form1()
+        {
+            InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+        }
+    }
+}
