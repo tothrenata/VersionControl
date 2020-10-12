32eec5d5f4e2bf5681d0e282c9e13499f50cfe56
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-10-12 20:48:17 +0200

    Webszolgáltatás hívása

--- 6.gyakorlat/6.gyakorlat/Form1.cs
+++ 6.gyakorlat/6.gyakorlat/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _6.gyakorlat.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,28 @@ namespace _6.gyakorlat
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+
+            ExchangeRates();
+
+        }
+
+        public void ExchangeRates()
+        {
+            var mbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
