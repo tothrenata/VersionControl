e38aae33eba774831e9d40d094f0886464a33832
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-10-12 21:26:27 +0200

    XML feldolgozása

--- 6.gyakorlat/6.gyakorlat/Form1.cs
+++ 6.gyakorlat/6.gyakorlat/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace _6.gyakorlat
 {
@@ -37,6 +38,24 @@ namespace _6.gyakorlat
 
             var response = mbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0) rate.Value = value / unit;
+            }
         }
     }
 }
