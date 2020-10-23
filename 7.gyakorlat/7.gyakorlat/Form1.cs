c8230d8f9768634e659686a796d212800b700ed3
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-10-23 16:30:43 +0200

    Szimuláció

--- 7.gyakorlat/7.gyakorlat/Form1.cs
+++ 7.gyakorlat/7.gyakorlat/Form1.cs
@@ -27,7 +27,23 @@ namespace _7.gyakorlat
             BirthProbabilities = GetBirthProbability(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbability(@"C:\Temp\halál.csv");
 
-            //dataGridView1.DataSource = BirthProbabilities;
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+
+                Console.WriteLine(string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
         public List<Person> GetPopulation(string csvpath)
         {
