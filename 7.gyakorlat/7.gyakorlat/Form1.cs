9eae42c1afef849c344116cab0ec5d76e2b5ffb3
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-10-23 16:55:33 +0200

    Szimulációs lépés

--- 7.gyakorlat/7.gyakorlat/Form1.cs
+++ 7.gyakorlat/7.gyakorlat/Form1.cs
@@ -31,7 +31,7 @@ namespace _7.gyakorlat
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    
+                    SimStep(year, Population[i]);
                 }
 
                 int nbrOfMales = (from x in Population
@@ -103,6 +103,36 @@ namespace _7.gyakorlat
             }
 
             return deathProbabilities;
+        }
+
+        public void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+
+            byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.pDeath).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.pBirth).FirstOrDefault();
+                
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
     }
 }
