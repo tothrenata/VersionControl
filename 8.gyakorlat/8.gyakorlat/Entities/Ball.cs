de2e39db4543df793dc9b13b2da603f64a7cd2de
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-11-10 19:33:39 +0100

    Labda létrehozása

---
+++ 8.gyakorlat/8.gyakorlat/Entities/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8.gyakorlat.Entities
+{
+    public class Ball: Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        private void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
