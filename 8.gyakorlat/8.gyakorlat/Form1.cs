d0547a5f86d42b50588ca6e1efc1ad9256b95bf2
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-11-10 21:09:57 +0100

    Színválasztó gomb

--- 8.gyakorlat/8.gyakorlat/Form1.cs
+++ 8.gyakorlat/8.gyakorlat/Form1.cs
@@ -76,5 +76,16 @@ namespace _8.gyakorlat
             _nextToy.Left = label1.Left;
             Controls.Add(_nextToy);
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
+        }
     }
 }
