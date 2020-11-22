c976893ba0eeba364a4e8ade427bcbd79d20c7fe
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-11-22 09:43:51 +0100

    Jelszó validálás teszt

--- 9.gyakorlat/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ 9.gyakorlat/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -28,5 +28,21 @@ namespace UnitTestExample.Test
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        [
+            Test,
+            TestCase("abcdABCD", false),
+            TestCase("ABCD1234", false),
+            TestCase("abcd1234", false),
+            TestCase("Abcd1234", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+            var actualResult = accountController.ValidatePassword(password);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        
     }
 }
