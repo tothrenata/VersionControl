5aa4b8074450cc71f65bbea672bdd8dce1294028
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-11-22 10:11:25 +0100

    Jelszó validálás függvény

--- 9.gyakorlat/UnitTestExample/Controllers/AccountController.cs
+++ 9.gyakorlat/UnitTestExample/Controllers/AccountController.cs
@@ -50,6 +50,30 @@ namespace UnitTestExample.Controllers
 
         public bool ValidatePassword(string password)
         {
+            if( !Regex.IsMatch(
+                password,
+                @"[a-z]"))
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(
+                password,
+                @"[A-Z]"))
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(
+                password,
+                @"[0-9]"))
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(
+                password,
+                @"[a-zA-Z0-9]{8,}"))
+            {
+                return false;
+            }
             return true;
         }
     }
