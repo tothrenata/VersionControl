6b5b32ac343b22c95d7b9834fa6b5e3fa52c57b9
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-11-22 10:55:21 +0100

    Register függvény javítása

--- 9.gyakorlat/UnitTestExample/Controllers/AccountController.cs
+++ 9.gyakorlat/UnitTestExample/Controllers/AccountController.cs
@@ -25,15 +25,16 @@ namespace UnitTestExample.Controllers
             if(!ValidateEmail(email))
                 throw new ValidationException(
                     "A megadott e-mail cím nem megfelelő!");
-            if(!ValidateEmail(email))
+            if(!ValidatePassword(password))
                 throw new ValidationException(
-                    "A megadottt jelszó nem megfelelő!\n" +
+                    "A megadott jelszó nem megfelelő!\n" +
                     "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
 
             var account = new Account()
             {
                 Email = email,
-                Password = password
+                Password = password,
+                ID = Guid.NewGuid()
             };
 
             var newAccount = AccountManager.CreateAccount(account);
