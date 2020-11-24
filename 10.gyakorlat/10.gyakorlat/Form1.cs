32808ea37bba4edeb33d95fd138571b53546ce0f
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-11-24 18:50:51 +0100

    Induló populáció felépítése

--- 10.gyakorlat/10.gyakorlat/Form1.cs
+++ 10.gyakorlat/10.gyakorlat/Form1.cs
@@ -16,6 +16,11 @@ namespace _10.gyakorlat
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +30,13 @@ namespace _10.gyakorlat
 
             gc.AddPlayer();
             gc.Start(true);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+
         }
     }
 }
