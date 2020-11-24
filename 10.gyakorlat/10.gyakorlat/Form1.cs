efba06f0af072c4c4cf5115eccf79fcc03f1aaaa
Renáta Tóth <renata.toth@stud.uni-corvinus.hu>
2020-11-24 18:47:41 +0100

    A játék kipróbálása

--- 10.gyakorlat/10.gyakorlat/Form1.cs
+++ 10.gyakorlat/10.gyakorlat/Form1.cs
@@ -22,6 +22,9 @@ namespace _10.gyakorlat
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
         }
     }
 }
